7c992caf054e807773c0372794a2cfecb840245f
Steve M Silva <silvasm516@aol.com>
2021-02-15 06:36:54 -0800

    Update settings.py

--- BP/settings.py
+++ BP/settings.py
@@ -127,6 +127,6 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static//'),
+    os.path.join(BASE_DIR, '/emerge/problemo/static/'),
 )
 
